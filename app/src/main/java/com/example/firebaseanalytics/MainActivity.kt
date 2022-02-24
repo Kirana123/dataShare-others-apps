c0cc986069e8d2f00d5bc0330302916779783f33
kirana123 <email@example.comtsanusha26@gmail.com>
2022-02-24 10:19:29 +0530

    Data share between other applications

---
+++ app/src/main/java/com/example/firebaseanalytics/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.firebaseanalytics
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+   // private FirebaseAnalytics firebaseanalytics;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.share_menu){
+            val shareIntent = Intent().apply {
+                this.action = Intent.ACTION_SEND
+                this.putExtra(Intent.EXTRA_TEXT, "Data sharing two between application.!")
+                this.type = "text/plain"
+            }
+            startActivity(shareIntent)
+        } else {
+            return super.onOptionsItemSelected(item)
+        }
+       return true
+    }
+}
